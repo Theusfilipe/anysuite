0d6a315fd674225c58f60f891c3755da6de6f08f
RCSSUBAQUATICA <ti@rcssubaquatica.com.br>
2025-02-26 16:22:05 -0300

    Developed the sidebar component on the package UI and used it on the MaintenanceLayout page

--- apps/web/app/maintenance/layout.tsx
+++ apps/web/app/maintenance/layout.tsx
@@ -1,40 +1,33 @@
 import { CSSProperties, ReactNode } from "react";
 import { getTranslation } from "./../../../../packages/i18n/index";
 
+
+
+import Sidebar from "@repo/ui/sidebar";
+
 import Link from "next/link";
 
 const lang = "pt";
 
+const menuItems = [
+  { href: "/maintenance/serviceRequestList", text: getTranslation(lang, "serviceRequestList") , icon: "Assignment" },
+  { href: "/maintenance/newServiceRequest", text: getTranslation(lang, "newServiceRequest"), icon:"PostAdd" },
+  { href: "/maintenance/newServiceOrder", text: getTranslation(lang, "newServiceOrder"), icon:"AssignmentReturn" },
+  { href: "/maintenance/serviceOrderList", text: getTranslation(lang, "serviceOrderList"), icon:"ListAlt"  },
+  { href: "/", text: getTranslation(lang, "return"),icon:"ArrowBack" },
+];
 export default function MaintenanceLayout({ children }: { children: ReactNode }) {
   return (
     <div style={{ display: "flex", height: "100vh" }}>
-      <aside style={sidebarStyle}>
-        <ul>
-          <li>
-            <Link href="/maintenance/serviceRequestList">{getTranslation(lang, "serviceRequestList")}</Link>
-          </li>
-          <li>
-            <Link href="/maintenance/newServiceRequest">{getTranslation(lang, "newServiceRequest")}</Link>
-          </li>
-          <li>
-            <Link href="/maintenance/newServiceOrder">{getTranslation(lang, "newServiceOrder")}</Link>
-          </li>
-          
-          <li>
-            <Link href="/maintenance/serviceOrderList">{getTranslation(lang, "serviceOrderList")}</Link>
-          </li>
-          <li>
-            <Link href="/">{getTranslation(lang, "return")}</Link>
-          </li>
-        </ul>
-      </aside>
+      
 
-      <main style={{ flex: 1, padding: "20px" }}>
+      <Sidebar title={getTranslation(lang, "maintenanceWelcome")} menuItems={menuItems}>
         {children} {/* Aqui o conteúdo das páginas filhas será exibido */}
-      </main>
+      </Sidebar>
     </div>
   );
 }
+
 
 const sidebarStyle: CSSProperties = {
   width: "200px",
@@ -46,3 +39,24 @@ const sidebarStyle: CSSProperties = {
   gap: "10px",
   borderRight: "2px solid #ddd",
 };
+
+//<aside style={sidebarStyle}>
+//        <ul>
+//          <li>
+//            <Link href="/maintenance/serviceRequestList">{getTranslation(lang, "serviceRequestList")}</Link>
+//          </li>
+//          <li>
+//            <Link href="/maintenance/newServiceRequest">{getTranslation(lang, "newServiceRequest")}</Link>
+//          </li>
+//          <li>
+//            <Link href="/maintenance/newServiceOrder">{getTranslation(lang, "newServiceOrder")}</Link>
+//          </li>
+//          
+//          <li>
+//            <Link href="/maintenance/serviceOrderList">{getTranslation(lang, "serviceOrderList")}</Link>
+//          </li>
+//          <li>
+//            <Link href="/">{getTranslation(lang, "return")}</Link>
+//          </li>
+//        </ul>
+//     </aside>
